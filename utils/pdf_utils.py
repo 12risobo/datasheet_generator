a2822de6f295d4b60886e0c56ce5b042bc146be6
risobo <michael.diks@gmail.com>
2025-02-19 17:10:32 +0100

    Property of ACT naar achtergrond en bestanden opgeschoond

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfReader, PdfWriter
+from copy import copy
 
 def add_watermark_to_pdf(input_pdf_path, watermark_pdf_path, output_pdf_path):
     """Add watermark from a PDF file to another PDF file."""
@@ -11,9 +12,11 @@ def add_watermark_to_pdf(input_pdf_path, watermark_pdf_path, output_pdf_path):
 
     # Apply watermark to each page
     for page in reader.pages:
-        page.merge_page(watermark)
-        writer.add_page(page)
+        # Create a fresh copy of the watermark for each page
+        watermark_copy = copy(watermark)
+        watermark_copy.merge_page(page)
+        writer.add_page(watermark_copy)
 
     # Write the output PDF
     with open(output_pdf_path, 'wb') as output_file:
-        writer.write(output_file) 
+        writer.write(output_file)
